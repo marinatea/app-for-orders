ac9fc3e9c8c7dd8229f34e1adf7c8a6a03cf9eaf
Maryna Karpova <mari.artarea@gmail.com>
2024-12-04 19:03:25 +0100

    railway db

--- pages/api/authenticate.ts
+++ pages/api/authenticate.ts
@@ -11,6 +11,7 @@ export default async function handler(
     console.error("Nieobsługiwana metoda:", req.method);
     return res.status(405).json({ message: "Metoda nieobsługiwana." });
   }
+  console.log('DATABASE_URL:', process.env.DATABASE_URL);  // Dodaj logowanie zmiennej środowiskowej
 
   const { codeToLogin } = req.body;
 
