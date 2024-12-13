fdfc4b9e36ea6107bce1abc2a9e50a4db8141400
Maryna Karpova <mari.artarea@gmail.com>
2024-12-13 12:28:59 +0100

    bottle

--- pages/Bottle.tsx
+++ pages/Bottle.tsx
@@ -3,8 +3,16 @@ import s from "../styles/Bottle.module.scss";
 
 export default function WineBottle() {
   return (
-    <div className={s.wineBottle}>
+    <div
+      className={s.wineBottle}
+    >
+      {/* Szyja */}
       <div className={s.neck}></div>
+
+      {/* Krążek */}
+      <div className={s.ring}></div>
+
+      {/* Korpus */}
       <div className={s.body}></div>
     </div>
   );
